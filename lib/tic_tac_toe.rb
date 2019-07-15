e2559325914e1b3e6158559219fae8aab7359a4b
Lantz Warrick <betalantz@gmail.com>
2019-07-15 00:02:20 +0000

    Automatically backed up by Learn

--- lib/tic_tac_toe.rb
+++ lib/tic_tac_toe.rb
@@ -34,4 +34,40 @@ class TicTacToe
     @board[index] = move
   end
 
+  def position_taken?(index)
+    @board[index] != " " ? true : false
+    # !(@board[index].nil? || @board[index] == " ")
+  end
+
+  def valid_move?(index)
+    index.between?(0, 8) && !self.position_taken?(index)
+    # @board[index] == " " ? true : false && index == (/[0-8]/)
+    # !self.position_taken(index)
+  end
+
+  def turn_count
+    @board.count {|pos| pos == "X" || pos == "O"}
+  end
+
+  def current_player
+    self.turn_count % 2 != 0 ? "O" : "X"
+  end
+
+  def turn
+    puts "Where would you like to move?"
+    input = gets.strip
+    index = self.input_to_index(input)
+    if self.valid_move?(index)
+      # player = self.current_player
+      self.move(index, self.current_player)
+      self.display_board
+    else
+      self.turn
+    end
+  end
+
+  def won?
+    WIN_COMBINATIONS.any? {|wins| wins.all?{|pos| @board[pos]=="X"|| @board[pos]=="O"}}
+  end
+
 end
